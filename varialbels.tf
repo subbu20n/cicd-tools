adc58345ea764c7ff890576790b26a01e9657e22
subbu <subbunandyala48@gmail.com>
2025-11-24 12:13:05 +0530

    cicd-tools

---
+++ varialbels.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "subbuaws.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z06528725AJCPEPL0K7K"
+  description = "description"
+}   
